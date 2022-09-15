b3e197b4f21a9b4ac214e42ba741fd9179d87626
Olena Voina <lena@kolpakov.org>
2022-09-15 21:36:03 +0200

    js from lection

---
+++ js/script1.js
@@ -0,0 +1,23 @@
+const nameOne = "Lena";
+
+console.log("Name: ", nameOne);
+
+const typeOfName = typeof nameOne;
+
+console.log(typeOfName);
+
+const numberOne = 15;
+
+console.log("Number: ", numberOne);
+
+const typeOfNumber = typeof numberOne;
+
+console.log(typeOfNumber);
+
+const shouldRenew = confirm("Продовжити підписку?");
+
+const typeOfRenew = typeof shouldRenew;
+
+console.log("Тип перемінної shouldRenew: ", typeOfRenew);
+
+console.log("Значення перемінної shouldRenew: ", shouldRenew);
